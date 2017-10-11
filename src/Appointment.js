bae4b7293784fb6708ea17ec65bfb81989d6d091
Simen Blikeng <simenblikeng@gmail.com>
2017-10-11 11:59:04 +0200

    fixed appointment.js and some css

--- src/Appointment.js
+++ src/Appointment.js
@@ -3,12 +3,13 @@ import { NavBar } from "./NavBar.js";
 import DatePickerS from "./Datepicker.js";
 import TimePickerS from "./Datepicker.js";
 import InputTodo from "./InputTodo.js";
+import FormAppoint from "./FormAppointment.js";
 
 const Appointment = () => (
   <div>
     <h1>Appointment page!</h1>
-    <InputTodo/>
-    <DatePickerS />
+    <FormAppoint />
+    
   </div>
 )
 
