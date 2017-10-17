ae5ac9a703f55a23dba5910ce01ab7a9e66f19b9
matssa <matssa@stud.ntnu.no>
2017-10-17 21:14:14 +0200

    add new test files, to be filled in

---
+++ __tests__/InputTodo.test.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { shallow } from "enzyme";
+import { removeItem } from "../src/Components/InputTodo.js";
+
+const startState = {
+	text: "",
+	items: [1, 2, 3]
+};
+
+const finState = startState.removeItem();
+
+expect(finState).toEqual([
+	{
+		text: "",
+		items: [1, 2]
+	}
+]);
